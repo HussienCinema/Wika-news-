54448e9212a1babed2343d5df46a5884dd5f4ef9
HussienCinema <tv.hussien.tv@gmail.com>
2026-01-20 06:59:13 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,10 @@
 const cacheName = 'wika-news-v1';
-const staticAssets = [
-  './',
-  './index.html',
-  './manifest.json'
-];
+const assets = ['./', './index.html', './manifest.json'];
 
-self.addEventListener('install', async el => {
-  const cache = await caches.open(cacheName);
-  await cache.addAll(staticAssets);
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
 });
 
-self.addEventListener('fetch', el => {
-  el.respondWith(
-    fetch(el.request).catch(() => caches.match(el.request))
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
